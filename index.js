26c60dc7e160fe8256bf6671f6d3728c2f12d939
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2021-03-20 23:30:57 -0500

    codegrade submission

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ console.log(cuboidTwo.surfaceArea());  // 130
 
 
 
-// 🦄 💪 Stretch Task: Extend the base clas CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area. 🦄 💪
+// 🦄 💪 Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area. 🦄 💪
   
 
 
