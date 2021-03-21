97c9ce4315f8e4b771e98552a344f523fe9d3159
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2021-03-20 21:59:42 -0500

    completed request 3

--- index.js
+++ index.js
@@ -87,8 +87,9 @@ const zooAnimals = [
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-  function lowPopulationAnimals(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowPopulationAnimals(arr){
+      const newArr = arr.filter(a => a.population < 5);
+      return newArr;
   }
   
 
